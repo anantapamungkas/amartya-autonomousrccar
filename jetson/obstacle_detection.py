0d1be01be19a215104cdab0e036eed89f476a7d4
anantapamungkas <anantavincent1@gmail.com>
2025-05-06 00:31:57 +0700

    initial commit

---
+++ jetson/obstacle_detection.py
@@ -0,0 +1,22 @@
+# trackbar green
+
+# trackbar red
+
+# trackbar yellow
+
+# read frame from video capture device
+
+# gausian blur frame
+
+# hsv & yuv value from trackbar
+
+# red segmentation
+# yellow segmentation
+# green segmentation
+    # get max contour
+        # draw bounding box
+
+# display
+
+
+
