dbedaaedca6b9e46c0d19156bc26f831da9d05c2
Juhdi <juhdi123456789@gmail.com>
2025-06-14 19:53:09 +0700

    encoder + button

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,18 +6,25 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Char.h>
 #include <std_msgs/Int32.h>
+#include <RotaryEncoder.h>
 
 //Declaration
 #define INR 0
 #define INL 2
 #define FreqM 100000
 #define ServoPin 12
-#define ButtonAPin 12
-#define ButtonAPin 12
-#define ButtonAPin 12
+#define ButtonAPin 24
+#define ButtonBPin 25
+#define ButtonCPin 26
+#define X_ENC1 20
+#define X_ENC2 21
 
 int motorSpeed, servoSteering;
+int posA;
+bool ButtonAState,ButtonBState,ButtonCState;
 
+RotaryEncoder *encoderA = nullptr;
+RotaryEncoder *encoderB = nullptr;
 Motor motor(INR,INL,FreqM);
 Servo servo;
 IMU IMU;
@@ -28,22 +35,24 @@ void msgSpeed(const std_msgs::Int32 &msg) {
 void msgSteering(const std_msgs::Int32 &msg) {
   servoSteering = msg.data;
 }
-
-void setup() {
-  Serial.begin(115200);
-  IMU.begin();
-  servo.write(90);
-  servo.attach(ServoPin);
+void checkPositionA() {
+  encoderA->tick();
 }
 
-void loop() {
-  
-}
 
 void display(){
   Serial.print(motorSpeed);
   Serial.print(" : ");
-  Serial.print(servoSteering);
+  Serial.println(servoSteering);
+}
+
+void sensor(){
+  encoderA->tick();
+  posA = encoderA->getPosition();
+
+  ButtonAState = digitalRead(ButtonAPin);
+  ButtonBState = digitalRead(ButtonBPin);
+  ButtonCState = digitalRead(ButtonCPin);
 }
 
 void aktuator(){
@@ -51,6 +60,32 @@ void aktuator(){
   servo.write(servoSteering);  
 }
 
-void Communicaton(){
+void communicaton(){
+  if (Serial.available() > 0) {
+    motorSpeed = Serial.readStringUntil(':').toInt();
+    servoSteering = Serial.readStringUntil(':').toInt();
+  }
+}
 
+void setup() {
+  Serial.begin(115200);
+  IMU.begin();
+  servo.write(90);
+  servo.attach(ServoPin);
+
+  encoderA = new RotaryEncoder(X_ENC1, X_ENC2, RotaryEncoder::LatchMode::TWO03);
+
+  attachInterrupt(digitalPinToInterrupt(X_ENC1), checkPositionA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(X_ENC2), checkPositionA, CHANGE);
+}
+
+void loop() {
+  communicaton();
+  display();
+  sensor();
+  aktuator();
+  if (ButtonAState == HIGH){
+    posA = 0;
+    delay(50);
+  }
 }
