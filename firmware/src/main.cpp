321ad03f0f847614bb75c8b4c6a3c0d6f330dbc4
Juhdi <juhdi123456789@gmail.com>
2025-06-25 10:45:39 +0700

    servo rosserial

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -12,18 +12,19 @@
 //Declaration
 #define INR 0
 #define INL 2
-#define FreqM 10000
-#define ServoPin 12
+#define FreqM 400
+#define ServoPin 3
 #define ButtonAPin 24
 #define ButtonBPin 25
 #define ButtonCPin 26
-#define X_ENC1 3
-#define X_ENC2 4
+#define X_ENC1 5
+#define X_ENC2 6
 
 int motorSpeed = 0, servoSteering=90;
 int posA;
 int ButtonAState,ButtonBState,ButtonCState;
 int heading;
+char buffer[50];
 
 RotaryEncoder *encoderA = nullptr;
 Motor motor(INR,INL,FreqM);
@@ -34,17 +35,12 @@ ros::NodeHandle nh;
 
 void velCallback(  const geometry_msgs::Twist& vel){
   motorSpeed = vel.linear.x ;
-  servoSteering = vel.angular.x ;
+  servoSteering = vel.angular.z ;
 }
 
-// void msgSpeed(const std_msgs::Int32 &msg) {
-//   motorSpeed = msg.data;
-// }
-// void msgSteering(const std_msgs::Int32 &msg) {
-//   servoSteering = msg.data;
-// }
-
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel" , velCallback);
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel" , velCallback);
 
 void checkPositionA() {
   encoderA->tick();
@@ -87,14 +83,14 @@ void setup() {
   servo.attach(ServoPin);
 
   nh.initNode();
+  nh.advertise(chatter);
   nh.subscribe(sub);
 
-  pinMode(ButtonAState, INPUT);
-  pinMode(ButtonBState, INPUT);
-  pinMode(ButtonCState, INPUT);
+  pinMode(ButtonAState, INPUT_PULLUP);
+  pinMode(ButtonBState, INPUT_PULLUP);
+  pinMode(ButtonCState, INPUT_PULLUP);
   servo.write(90);
   
-
   encoderA = new RotaryEncoder(X_ENC1, X_ENC2, RotaryEncoder::LatchMode::TWO03);
 
   attachInterrupt(digitalPinToInterrupt(X_ENC1), checkPositionA, CHANGE);
@@ -107,20 +103,31 @@ void loop() {
   sensor();
   // aktuator();
 
-  if (ButtonAState == 1023){
-    servoSteering += 5;
-    delay(50);
-  }
+  // if (ButtonAState == 1023){
+  //   servoSteering += 5;
+  //   delay(50);
+  // }
 
-  if (ButtonBState == 1023){
-    servoSteering -= 5;
-    delay(50);
-  }
+  // if (ButtonBState == 1023){
+  //   servoSteering -= 5;
+  //   delay(50);
+  // }
 
-  Serial.println(posA);
   servoSteering = constrain(servoSteering,60,120);
-  // servo.write(servoSteering); 
+  servo.write(heading); 
+
+  sprintf(buffer, "motorSpeed: %d, servoSteering: %d", motorSpeed, servoSteering);
+  str_msg.data = buffer;
+  chatter.publish( &str_msg );
+
+  Serial.print("motorSpeed ");
+  Serial.print(motorSpeed);
+  Serial.print(" : ");
+  Serial.print("servoSteering ");
+  Serial.print(servoSteering);
+  Serial.println();
   //motor.setSpeed(20);//15 ws banter //Servo e sudut tengah e 90, mentok kiri 60 deraat, mentok kanan 120 derajat. jangan melebihi rntang 
 
   nh.spinOnce();
+  delay(1);
 }
